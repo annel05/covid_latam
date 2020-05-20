31bb0a96158c74e10ee6601f2ce6028ce5bcd8ed
Lenny Martinez <lennymartinezd@gmail.com>
2020-05-20 15:45:04 -0400

    start working on scatterplot

---
+++ scatterplot_indices/draw_scatter.js
@@ -0,0 +1,15 @@
+async function drawScatter() {
+  // 1. access data
+  let dataset = await d3.csv('./../data/mexico-20200508.csv');
+
+  // const xAccessor = d => d.dewPoint;
+  // const yAccessor = d => d.humidity;
+  // console.log(dataset);
+  // 2. create dimensions
+  // 3. draw canvas
+  // 4. create scales
+  // 5. draw data
+  // 6. draw peripherals
+  // 7. set up interactions
+}
+drawScatter();
