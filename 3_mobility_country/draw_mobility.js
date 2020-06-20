915bfc985260226f3895b6a69a2bece4d57bd545
Lenny Martinez <lennymartinezd@gmail.com>
2020-06-20 13:56:52 -0400

    fix country column

--- 3_mobility_country/draw_mobility.js
+++ 3_mobility_country/draw_mobility.js
@@ -12,9 +12,10 @@ async function drawMobility() {
   setLocale();
 
   // 1. access data
-  const dataset_all = await d3.csv(
-    'https://raw.githubusercontent.com/lennymartinez/covid_latam/master/data/data_latest.csv'
-  );
+  // const dataset_all = await d3.csv(
+  //   'https://raw.githubusercontent.com/lennymartinez/covid_latam/master/data/data_latest.csv'
+  // );
+  const dataset_all = await d3.csv('./../data/data_latest.csv');
   const dataset = dataset_all.filter(d => d.country == 'Mexico');
 
   // data accessors, shorthand for different columns
