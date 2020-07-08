8cce37472799277eefc41c0a6810cbe2d08df839
Lenny Martinez <lennymartinezd@gmail.com>
2020-07-08 15:43:38 -0400

    tweak scatterplot

--- 99_scatterplot/draw_scatter.js
+++ 99_scatterplot/draw_scatter.js
@@ -1,28 +1,19 @@
 async function drawScatter() {
-  // 1. access data
-  let dataset = await d3.csv(
-    'https://raw.githubusercontent.com/lennymartinez/covid_latam/master/data/mexico-20200519.csv?token=AB4JHMMEPKKOU7VG7PMKPTC62LJ3K'
-  );
+  // 1. Access data
 
-  const xAccessor = d => +d.Policy_Index_Adjusted_Time;
-  const yAccessor = d => +d.avg_google_7d;
+  const dataset = await d3.csv('./../data/mexico_data_latest.csv');
+
+  const xAccessor = d => +d.policy_index;
+  const yAccessor = d => +d.mobility_index;
   const dateParser = d3.timeParse('%d-%b-%y');
-  const dateAccessor = d => dateParser(d.Date);
-  const dayAccessor = d => +d.Days;
+  const dateAccessor = d => dateParser(d.date);
+  const dayAccessor = d => +d.days;
 
   const latestDay = d3.max(dataset.map(dayAccessor));
   const firstDay = d3.min(dataset.map(dayAccessor));
 
-  // set slider maximum
-  let slider = d3.select('#date_range');
-  slider.attr('max', latestDay).attr('value', latestDay);
+  // 2. Create chart dimensions
 
-  const updateTransition = d3.transition().duration(600);
-
-  let nestedDataset = d3.nest().key(dayAccessor).entries(dataset);
-  let data = nestedDataset[latestDay - 1];
-
-  // 2. create dimensions
   const width = d3.min([window.innerWidth * 0.9, window.innerHeight * 0.9]);
 
   let dimensions = {
@@ -32,7 +23,7 @@ async function drawScatter() {
       top: 10,
       right: 10,
       bottom: 50,
-      left: 60,
+      left: 50,
     },
   };
 
@@ -41,7 +32,8 @@ async function drawScatter() {
   dimensions.boundedHeight =
     dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
-  // 3. draw canvas
+  // 3. Draw canvas
+
   const wrapper = d3
     .select('#wrapper')
     .append('svg')
@@ -85,154 +77,44 @@ async function drawScatter() {
     .style('text-anchor', 'middle')
     .style('font-size', '1.4em');
 
-  // 4. create scales
+  // 4. Create scales
+
   const xScale = d3
     .scaleLinear()
-    .domain(d3.extent(data.values, xAccessor))
+    .domain(d3.extent(dataset, xAccessor))
     .range([0, dimensions.boundedWidth])
     .nice();
 
   const yScale = d3
     .scaleLinear()
-    .domain(d3.extent(data.values, yAccessor))
+    .domain(d3.extent(dataset, yAccessor))
     .range([dimensions.boundedHeight, 0])
     .nice();
 
-  // 5. draw data
-  const drawDots = _dataset => {
-    const dots = bounds.selectAll('circle').data(_dataset);
+  // 5. Draw data
+
+  const drawDots = dataset => {
+    const dots = bounds.selectAll('circle').data(dataset, d => d[0]);
+
     const newDots = dots
       .enter()
       .append('circle')
-      .attr('r', 0)
+      .filter(d => +d.days == latestDay);
+
+    const allDots = newDots
+      .merge(dots)
       .attr('cx', d => xScale(xAccessor(d)))
       .attr('cy', d => yScale(yAccessor(d)))
-      .attr('fill', '#f2f2f2')
-      .attr('stroke', '#333333');
+      .attr('r', 4);
 
-    const allDots = newDots.merge(dots);
-
-    allDots
-      .attr('cx', d => xScale(xAccessor(d)))
-      .attr('cy', d => yScale(yAccessor(d)))
-      .attr('r', 7);
-
-    const oldDots = dots
-      .exit()
-      .attr('fill', 'red')
-      .transition(updateTransition)
-      .attr('r', 0)
-      .remove();
+    const oldDots = dots.exit().remove();
   };
-  drawDots(data.values);
+
+  drawDots(dataset);
 
   // 6. Draw peripherals
-  const meanX = d3.mean(data.values, xAccessor);
-  const meanY = d3.mean(data.values, yAccessor);
 
-  const drawMean = _dataset => {
-    const meanLineX = bounds
-      .selectAll('.mean_x')
-      .attr('x1', xScale(meanX))
-      .attr('x2', xScale(meanX))
-      .attr('y1', 0)
-      .attr('y2', dimensions.boundedHeight);
-
-    const meanLineY = bounds
-      .selectAll('.mean_y')
-      .attr('y1', yScale(meanY))
-      .attr('y2', yScale(meanY))
-      .attr('x1', 0)
-      .attr('x2', dimensions.boundedWidth);
-  };
-
-  drawMean(data.values);
-
-  const drawAxes = _dataset => {
-    const xAxisGenerator = d3
-      .axisBottom()
-      .scale(xScale)
-      .tickSize(-dimensions.boundedWidth - dimensions.margin.top);
-
-    const xAxis = bounds
-      .select('.x_axis')
-      .transition(updateTransition)
-      .call(xAxisGenerator);
-
-    xAxis.selectAll('g.tick text').attr('dy', '1.25em');
-    const xAxisLabel = xAxis
-      .select('.x_axis_label')
-      .text('Indice de adopcion de politicas');
-
-    const yAxisGenerator = d3
-      .axisLeft()
-      .scale(yScale)
-      .tickFormat(d => d + '%')
-      .tickSize(-(dimensions.width + dimensions.margin.left));
-
-    const yAxis = bounds
-      .select('.y_axis')
-      .transition(updateTransition)
-      .call(yAxisGenerator);
-
-    const yAxisLabel = yAxis
-      .select('.y_axis_label')
-      .text('Porcentaje de caída de la movilidad');
-  };
-
-  drawAxes(data.values);
-
-  const drawQuadrants = _dataset => {
-    const upper_left = bounds
-      .select('.upper_left')
-      .attr('x', 0)
-      .attr('y', 0)
-      .attr('width', xScale(meanX))
-      .attr('height', yScale(meanY));
-
-    const upper_right = bounds
-      .select('.upper_right')
-      .attr('x', xScale(meanX))
-      .attr('y', 0)
-      .attr('width', dimensions.boundedWidth - xScale(meanX))
-      .attr('height', yScale(meanY));
-    upper_right.transition(updateTransition).attr('fill', '#56AD9E');
-
-    upper_left.transition(updateTransition).attr('fill', '#E97F63');
-
-    const lower_left = bounds
-      .select('.lower_left')
-      .attr('x', 0)
-      .attr('y', yScale(meanY))
-      .attr('width', xScale(meanX))
-      .attr('height', dimensions.boundedHeight - yScale(meanY));
-
-    lower_left.transition(updateTransition).attr('fill', '#F3B9A2');
-
-    const lower_right = bounds
-      .select('.lower_right')
-      .attr('x', xScale(meanX))
-      .attr('y', yScale(meanY))
-      .attr('width', dimensions.boundedWidth - xScale(meanX))
-      .attr('height', dimensions.boundedHeight - yScale(meanY));
-
-    lower_right.transition(updateTransition).attr('fill', '#488D81');
-  };
-  drawQuadrants(data.values);
-
-  // 7. add interactivity
-  // slider.on('input', changeDay);
-  // function changeDay() {
-  //   console.log(this.value);
-  //   const newDataset = nestedDataset[this.value - 1];
-
-  //   xScale.domain(d3.extent(newDataset, xAccessor));
-  //   yScale.domain(d3.extent(newDataset, yAccessor));
-  //   drawAxes(newDataset);
-  //   // TODO Update dots
-  //   // TODO Update Axes
-  //   // TODO Update Mean Lines
-  //   // TODO Update quadrants
-  // }
+  // 7. Set up interactions
 }
+
 drawScatter();
