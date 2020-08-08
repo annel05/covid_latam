3b66615ed51ca90122a76a49ca56986de6ee9284
Lenny Martinez <lennymartinezd@gmail.com>
2020-08-08 16:50:39 -0400

    work on map start

--- 03-ihme-deaths-projections/ihme_projections.js
+++ 03-ihme-deaths-projections/ihme_projections.js
@@ -170,11 +170,11 @@ async function ihmeChart() {
   });
 
   const projectBoundaries = function () {
-    // TODO select #cutoffDate_line and remove()
-    d3.select('#cutOffDate_line').remove()
-    // TODO select #projectionBox and remove()
-    d3.select('#projectionBox').remove()
-    // TODO draw projectionBox: Rectangle with grey low opacity. X1 is cutoffdate, y1 is top ?. Width is something? xScale.range()[1] - xScale(cutoffDate) and height is dimensions.boundedHeight
+    // select #cutoffDate_line and remove()
+    d3.select('#cutOffDate_line').remove();
+    // select #projectionBox and remove()
+    d3.select('#projectionBox').remove();
+    // draw projectionBox: Rectangle with grey low opacity. X1 is cutoffdate, y1 is top ?. Width is something? xScale.range()[1] - xScale(cutoffDate) and height is dimensions.boundedHeight
     bounds
       .append('rect')
       .attr('id', '#projectionBox')
@@ -208,8 +208,12 @@ async function ihmeChart() {
       d => locationIDAccessor(d) == _locationID
     );
     // segment data into real and projection
-    const confirmedData = countrySpecific.filter(d => xAccessor(d) <= cutoffDate);
-    const projectionData = countrySpecific.filter(d => xAccessor(d) > cutoffDate);
+    const confirmedData = countrySpecific.filter(
+      d => xAccessor(d) <= cutoffDate
+    );
+    const projectionData = countrySpecific.filter(
+      d => xAccessor(d) > cutoffDate
+    );
     // draw area
     bounds
       .append('path')
@@ -217,7 +221,7 @@ async function ihmeChart() {
       .attr('fill-opacity', 0.15)
       .attr('stroke', 'none')
       .attr('d', areaGenerator(countrySpecific));
-    // TODO draw confirmed line
+    // draw confirmed line
     bounds
       .append('path')
       .attr('fill', 'none')
@@ -226,7 +230,7 @@ async function ihmeChart() {
       .attr('stroke-width', 1.25)
       .attr('stroke', colorScale(countryNameAccessor(countrySpecific[0])))
       .attr('d', d => lineGenerator(confirmedData));
-    // TODO draw projection line
+    // draw projection line
     bounds
       .append('path')
       .attr('fill', 'none')
@@ -244,35 +248,37 @@ async function ihmeChart() {
   const ownCountries = ['Mexico', 'Brazil', 'Bolivia', 'Chile'];
   const ownCountryIdArray = [];
   ownCountries.forEach(_element => {
-    // TODO filter data for that country
-    const filtered = countryData.filter(d => countryNameAccessor(d) == _element);
-    // TODO get the locationID for the first row
+    // filter data for that country
+    const filtered = countryData.filter(
+      d => countryNameAccessor(d) == _element
+    );
+    // get the locationID for the first row
     const filteredId = locationIDAccessor(filtered[0]);
-    // TODO push locationIDs to a new array
+    // push locationIDs to a new array
     ownCountryIdArray.push(filteredId);
   });
 
   // 7. act interactivity
 
   // const state_list = d3
-  //   .select('#state_list_policy')
-  //   .selectAll('input')
-  //   .data(states)
-  //   .enter()
-  //   .append('li')
-  //   .attr('class', d => `${stateCodeAccessor(d.values[0])}_input`);
+  // .select('#state_list_policy')
+  // .selectAll('input')
+  // .data(states)
+  // .enter()
+  // .append('li')
+  // .attr('class', d => `${stateCodeAccessor(d.values[0])}_input`);
 
   // state_list
-  //   .append('input')
-  //   .attr('class', 'input_box_policy')
-  //   .attr('type', 'checkbox')
-  //   .attr('name', d => `${stateCodeAccessor(d.values[0])}_policy`);
+  // .append('input')
+  // .attr('class', 'input_box_policy')
+  // .attr('type', 'checkbox')
+  // .attr('name', d => `${stateCodeAccessor(d.values[0])}_policy`);
 
   // state_list
-  //   .append('label')
-  //   .attr('class', 'input_label')
-  //   .attr('for', d => `${stateCodeAccessor(d.values[0])}_policy`)
-  //   .html(d => stateAccessor(d.values[0]));
+  // .append('label')
+  // .attr('class', 'input_label')
+  // .attr('for', d => `${stateCodeAccessor(d.values[0])}_policy`)
+  // .html(d => stateAccessor(d.values[0]));
 
   ownCountryIdArray.forEach(_element => {
     activateCountry(_element);
@@ -282,178 +288,178 @@ async function ihmeChart() {
 
   // state_list.select(`[name=${firstRankCode}_policy]`).property('checked', true);
   // state_list
-  //   .select(`[for=${firstRankCode}_policy]`)
-  //   .style('color', colorScale(firstRankCode))
-  //   .style('font-weight', 'bold');
+  // .select(`[for=${firstRankCode}_policy]`)
+  // .style('color', colorScale(firstRankCode))
+  // .style('font-weight', 'bold');
 
   // state_list.select(`[name=${lastRankCode}_policy]`).property('checked', true);
   // state_list
-  //   .select(`[for=${lastRankCode}_policy]`)
-  //   .style('color', colorScale(lastRankCode))
-  //   .style('font-weight', 'bold');
+  // .select(`[for=${lastRankCode}_policy]`)
+  // .style('color', colorScale(lastRankCode))
+  // .style('font-weight', 'bold');
 
   // d3.selectAll('.input_box_policy').on('input', toggleStateLine);
 
   // function toggleStateLine() {
-  //   const code = this.name.split('_')[0];
-  //   const label = state_list.select(`[for=${this.name}]`);
-  //   if (this.checked) {
-  //     // input box has been checked
-  //     // 1 - turn on state line
-  //     addStateLine(code);
-  //     // 2 - turn on label to match color
-  //     label.style('color', colorScale(code)).style('font-weight', 'bold');
-  //   } else {
-  //     // input box has been unchecked
-  //     // 1 - turn off state line
-  //     bounds.select(`.${code}_temp_policy`).remove();
-  //     label.style('color', '#000').style('font-weight', 'normal');
-  //     // 2 - turn off label to match colors
-  //   }
+  // const code = this.name.split('_')[0];
+  // const label = state_list.select(`[for=${this.name}]`);
+  // if (this.checked) {
+  // // input box has been checked
+  // // 1 - turn on state line
+  // addStateLine(code);
+  // // 2 - turn on label to match color
+  // label.style('color', colorScale(code)).style('font-weight', 'bold');
+  // } else {
+  // // input box has been unchecked
+  // // 1 - turn off state line
+  // bounds.select(`.${code}_temp_policy`).remove();
+  // label.style('color', '#000').style('font-weight', 'normal');
+  // // 2 - turn off label to match colors
+  // }
   // }
   // const tooltipDate = bounds
-  //   .append('text')
-  //   .attr('class', 'tooltipDate_policy')
-  //   .style('opacity', 0);
+  // .append('text')
+  // .attr('class', 'tooltipDate_policy')
+  // .style('opacity', 0);
   // // tooltip interactivity:
   // listeningRect.on('mousemove', onMouseMove).on('mouseleave', onMouseLeave);
 
   // const tooltip = d3
-  //   .select('#tooltip_policy')
-  //   .style('opacity', 0)
-  //   .style('top', `${dimensions.margin.top * 2}px`)
-  //   .style('left', `${dimensions.margin.left * 1.25}px`);
+  // .select('#tooltip_policy')
+  // .style('opacity', 0)
+  // .style('top', `${dimensions.margin.top * 2}px`)
+  // .style('left', `${dimensions.margin.left * 1.25}px`);
   // const tooltipHeader = tooltip.select('#tooltipHeader_policy');
   // const tooltipContent = tooltip.select('#tooltipContent_policy');
   // let activeStates;
 
   // function onMouseMove() {
-  //   tooltip.style('opacity', 1);
-  //   // Translate mouse position into a date and y-value
-  //   const mousePosition = d3.mouse(this);
-  //   const hoveredDate = xScale.invert(mousePosition[0]);
+  // tooltip.style('opacity', 1);
+  // // Translate mouse position into a date and y-value
+  // const mousePosition = d3.mouse(this);
+  // const hoveredDate = xScale.invert(mousePosition[0]);
 
-  //   const getDistanceFromHoveredDate = d =>
-  //     Math.abs(xAccessor(d) - hoveredDate);
+  // const getDistanceFromHoveredDate = d =>
+  // Math.abs(xAccessor(d) - hoveredDate);
 
-  //   const closestIndex = d3.scan(
-  //     dataset,
-  //     (a, b) => getDistanceFromHoveredDate(a) - getDistanceFromHoveredDate(b)
-  //   );
-  //   const closestDate = dataset[closestIndex];
+  // const closestIndex = d3.scan(
+  // dataset,
+  // (a, b) => getDistanceFromHoveredDate(a) - getDistanceFromHoveredDate(b)
+  // );
+  // const closestDate = dataset[closestIndex];
 
-  //   const data = states.filter(d => d.date == closestDate.date);
-  //   const closestXValue = xAccessor(closestDate);
-  //   const closestYValue = yAccessor(closestDate);
+  // const data = states.filter(d => d.date == closestDate.date);
+  // const closestXValue = xAccessor(closestDate);
+  // const closestYValue = yAccessor(closestDate);
 
-  //   activeStates = ['Nacional'];
-  //   // get a list of all the active states
-  //   const allActive = document
-  //     .getElementById('wrapper_policy_main')
-  //     .getElementsByClassName('active_policy');
+  // activeStates = ['Nacional'];
+  // // get a list of all the active states
+  // const allActive = document
+  // .getElementById('wrapper_policy_main')
+  // .getElementsByClassName('active_policy');
 
-  //   Array.from(allActive).forEach(element => {
-  //     code = element.getAttribute('class').split('_')[0];
-  //     activeStates.push(code);
-  //   });
+  // Array.from(allActive).forEach(element => {
+  // code = element.getAttribute('class').split('_')[0];
+  // activeStates.push(code);
+  // });
 
-  //   // clear the tooltip box
-  //   tooltipHeader.selectAll('*').remove();
-  //   tooltipContent.selectAll('*').remove();
-  //   d3.selectAll('.temp_circle_policy').remove();
+  // // clear the tooltip box
+  // tooltipHeader.selectAll('*').remove();
+  // tooltipContent.selectAll('*').remove();
+  // d3.selectAll('.temp_circle_policy').remove();
 
-  //   const displayFormat = d3.timeFormat('%d %B');
+  // const displayFormat = d3.timeFormat('%d %B');
 
-  //   // Update tooltipDate with current date:
-  //   tooltipDate
-  //     .attr('x', xScale(closestXValue) + 15)
-  //     .attr('y', mousePosition[1])
-  //     .text(displayFormat(dateParser(closestDate.date)))
-  //     .attr('font-weight', 700)
-  //     .style('opacity', 1);
-  //   // Add date to tooltip
-  //   tooltipHeader
-  //     .append('span')
-  //     .html(displayFormat(dateParser(closestDate.date)));
+  // // Update tooltipDate with current date:
+  // tooltipDate
+  // .attr('x', xScale(closestXValue) + 15)
+  // .attr('y', mousePosition[1])
+  // .text(displayFormat(dateParser(closestDate.date)))
+  // .attr('font-weight', 700)
+  // .style('opacity', 1);
+  // // Add date to tooltip
+  // tooltipHeader
+  // .append('span')
+  // .html(displayFormat(dateParser(closestDate.date)));
 
-  //   tooltipLine
-  //     .attr('x1', xScale(closestXValue))
-  //     .attr('x2', xScale(closestXValue))
-  //     .attr('y1', 0)
-  //     .attr('y2', dimensions.boundedHeight)
-  //     .attr('stroke-width', 2)
-  //     .attr('stroke-dasharray', '7px 2px')
-  //     .attr('stroke', '#000')
-  //     .style('opacity', 1);
+  // tooltipLine
+  // .attr('x1', xScale(closestXValue))
+  // .attr('x2', xScale(closestXValue))
+  // .attr('y1', 0)
+  // .attr('y2', dimensions.boundedHeight)
+  // .attr('stroke-width', 2)
+  // .attr('stroke-dasharray', '7px 2px')
+  // .attr('stroke', '#000')
+  // .style('opacity', 1);
 
-  //   // add value for each active state to the tooltip
-  //   activeStates.forEach(element => {
-  //     // filter for the state's data on that day.
-  //     const point = dataset
-  //       .filter(d => d.state_short == element)
-  //       .filter(d => d.date == closestDate.date);
+  // // add value for each active state to the tooltip
+  // activeStates.forEach(element => {
+  // // filter for the state's data on that day.
+  // const point = dataset
+  // .filter(d => d.state_short == element)
+  // .filter(d => d.date == closestDate.date);
 
-  //     const yValue = yAccessor(point[0]);
-  //     const xValue = xAccessor(point[0]);
-  //     const stateName = stateCodeAccessor(point[0]);
-  //     const getColor = _code => {
-  //       if (_code == 'Nacional') {
-  //         return '#171717';
-  //       } else {
-  //         return colorScale(stateName);
-  //       }
-  //     };
-  //     const pointInfo = tooltipContent
-  //       .append('tr')
-  //       .attr('class', 'tooltip_state');
-  //     pointInfo
-  //       .append('td')
-  //       .attr('class', 'tooltip_state_name')
-  //       .html(point[0].state_name)
-  //       .style('color', getColor(element));
-  //     pointInfo
-  //       .append('td')
-  //       .attr('class', 'tooltip_value')
-  //       .html(yValue.toFixed(1));
+  // const yValue = yAccessor(point[0]);
+  // const xValue = xAccessor(point[0]);
+  // const stateName = stateCodeAccessor(point[0]);
+  // const getColor = _code => {
+  // if (_code == 'Nacional') {
+  // return '#171717';
+  // } else {
+  // return colorScale(stateName);
+  // }
+  // };
+  // const pointInfo = tooltipContent
+  // .append('tr')
+  // .attr('class', 'tooltip_state');
+  // pointInfo
+  // .append('td')
+  // .attr('class', 'tooltip_state_name')
+  // .html(point[0].state_name)
+  // .style('color', getColor(element));
+  // pointInfo
+  // .append('td')
+  // .attr('class', 'tooltip_value')
+  // .html(yValue.toFixed(1));
 
-  //     // add a dot for each state
-  //     bounds
-  //       .append('circle')
-  //       .attr('cx', xScale(xValue))
-  //       .attr('cy', yScale(yValue))
-  //       .attr('r', 7)
-  //       .attr('fill', getColor(element))
-  //       .attr('class', 'temp_circle_policy');
-  //   });
+  // // add a dot for each state
+  // bounds
+  // .append('circle')
+  // .attr('cx', xScale(xValue))
+  // .attr('cy', yScale(yValue))
+  // .attr('r', 7)
+  // .attr('fill', getColor(element))
+  // .attr('class', 'temp_circle_policy');
+  // });
 
-  //   //
+  // //
   // }
 
   // function onMouseLeave() {
-  //   activeStates = ['Nacional'];
-  //   tooltip.style('opacity', 0);
-  //   tooltipLine.style('opacity', 0);
-  //   bounds.selectAll('.temp_circle_policy').remove();
-  //   tooltipDate.style('opacity', 0);
+  // activeStates = ['Nacional'];
+  // tooltip.style('opacity', 0);
+  // tooltipLine.style('opacity', 0);
+  // bounds.selectAll('.temp_circle_policy').remove();
+  // tooltipDate.style('opacity', 0);
   // }
 
   // d3.selectAll('.states').on('click', toggleStateLineManually);
   // d3.selectAll('.active_policy').on('click', toggleStateLineManually);
   // function toggleStateLineManually() {
-  //   const ourClass = this.classList[0];
-  //   const code = ourClass.split('_')[0];
-  //   // find the input box associated with this line
-  //   const inputBox = d3.select(`[name=${code}_policy`);
-  //   const isActive = inputBox._groups[0][0].checked;
-  //   const label = d3.select(`[for=${code}_policy]`);
-  //   if (isActive) {
-  //     // remove the state line, turn off the label, and uncheck the box
-  //   } else {
-  //     // draw the state line, turn on the label, and check the box
-  //     addStateLine(code);
-  //     label.style('color', colorScale(code)).style('font-weight', 'bold');
-  //     inputBox.property('checked', true);
-  //   }
+  // const ourClass = this.classList[0];
+  // const code = ourClass.split('_')[0];
+  // // find the input box associated with this line
+  // const inputBox = d3.select(`[name=${code}_policy`);
+  // const isActive = inputBox._groups[0][0].checked;
+  // const label = d3.select(`[for=${code}_policy]`);
+  // if (isActive) {
+  // // remove the state line, turn off the label, and uncheck the box
+  // } else {
+  // // draw the state line, turn on the label, and check the box
+  // addStateLine(code);
+  // label.style('color', colorScale(code)).style('font-weight', 'bold');
+  // inputBox.property('checked', true);
+  // }
   // }
 }
 ihmeChart();
--- 04-choropleth/choropleth-map.js
+++ 04-choropleth/choropleth-map.js
@@ -1,7 +1,27 @@
 async function drawMap() {
   // 1. load data
-  // TODO load country shapes
-  // TODO load JHU dataset
+  // load country shapes
+  // TODO change to latam geojson
+  const countryShapes = await d3.json('./../data/world-geojson.json');
+  console.log(countryShapes);
 
-  // Create 
+  // TODO load JHU dataset
+  // step 1. calculate yesterday's date
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(yesterday.getDate() - 1);
+  const formatTime = d3.timeFormat('%m-%d-%Y');
+
+  // step 2. turn it into a string of format mm-dd-yyyy
+  const date = formatTime(yesterday);
+  // step 3. feed it into url
+  const dataset = await d3.csv(
+    `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/${date}.csv`
+  );
+
+  // data accessors
+  const countryNameAccessor = d.properties['Country_Region'];
+  const provinceNameAccessor = d.properties['Province_State'];
+  const combinedKeyAccessor = d.properties['Combined_Key'];
 }
+drawMap();
